0593ce8d6aadf146de6be84ba67811c5f4bd972f
Archie-f <akif.mk66@gmail.com>
2022-01-12 05:45:58 +0300

    MainApi Explanation added

--- src/test/java/com/testIT/pages/MainApi.java
+++ src/test/java/com/testIT/pages/MainApi.java
@@ -10,11 +10,22 @@ public class MainApi {
     static Response response;
     static ToDo toDo;
 
-    public static void sendGetRequest(String endPonit){
+    /**
+     * Sends a GET request with the given end point
+     *
+     * @param endPoint
+     */
+    public static void sendGetRequest(String endPoint){
         response = RestAssured.given().accept(ContentType.JSON)
-                .when().get(endPonit);
+                .when().get(endPoint);
     }
 
+    /**
+     * Sends a POST request with the given POJO object and end point
+     *
+     * @param toDo
+     * @param endPoint
+     */
     public static void sendPostRequest(Object toDo, String endPoint) {
         response = RestAssured.given().accept(ContentType.JSON)
                 .and().contentType(ContentType.JSON)
@@ -22,23 +33,45 @@ public class MainApi {
                 .when().post(endPoint);
     }
 
+    /**
+     * Sends a PUT request using the POJO object with modified data and end point
+     *
+     * @param toDo
+     * @param endPoint
+     */
     public static void sendPutRequest(Object toDo, String endPoint){
         response = RestAssured.given().contentType(ContentType.JSON)
                 .and().body(toDo)
                 .when().put(endPoint);
     }
 
+    /**
+     * Sends a PATCH request using the patch String with modified data and end point
+     *
+     * @param patch
+     * @param endPoint
+     */
     public static void sendPatchRequest(String patch, String endPoint){
         response =  RestAssured.given().contentType(ContentType.JSON)
                 .and().body(patch)
                 .when().patch(endPoint);
     }
 
+    /**
+     * Sends a DELETE request using the end point
+     *
+     * @param endPoint
+     */
     public static void sendDeleteRequest(String endPoint){
         response = RestAssured.given().accept(ContentType.JSON)
                 .when().delete(endPoint);
     }
 
+    /**
+     * Returns the response object
+     *
+     * @return response
+     */
     public static Response getResponse() {
         return response;
     }
@@ -52,6 +85,11 @@ public class MainApi {
         toDo = new ToDo(userID,title,todoComplete);
     }
 
+    /**
+     * Returns the toDo object
+     *
+     * @return toDo
+     */
     public static ToDo getToDo(){
         return toDo;
     }
